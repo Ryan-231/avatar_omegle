850ead9019230707fdfe8d2e95606afd6e5f528b
Siddhi Mundhra <siddhimundhra@169-231-64-34.wireless.ucsb.edu>
2025-01-11 18:11:28 -0800

    new commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DATABASE = 'matching_app'
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')  # Use environment variable, default to localhost
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')      # Use environment variable, default to root
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')  # Use environment variable, default to empty string
+    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'matching_app')  # Use environment variable, default to 'matching_app'
+
